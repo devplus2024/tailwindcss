4c6f26521be0c52e7ac3b9d887b989a3782b4a0e
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-01-30 17:23:16 +0700

    comit-10

--- script/script.js
+++ script/script.js
@@ -1,2 +1,18 @@
-var welcome = "JavaScript"
-document.write(welcome," Hello");
+function layDuLieu() {
+  var giaTriInput1 = document.getElementById("myInput1").value;
+  var giaTriInput2 = document.getElementById("myInput2").value;
+  var giaTriInput3 = document.getElementById("myInput3").value;
+
+  if (
+    giaTriInput1 == "codeder@gmail.com" &&
+    giaTriInput2 == "coder123" &&
+    giaTriInput3 == "08112009"
+  ) {
+    console.log("Dữ liệu từ ô input: " + giaTriInput1);
+    window.location.href =
+      "https://phamquangtruongan.github.io/tailwindcss/";
+  } else {
+    console.log("Error");
+  }
+}
+
