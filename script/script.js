e9fe11d340d71ce9986409fc7a3b6b2a6b40d0a5
Phạm Quang Trường An <anpham08112009@gmail.com>
2024-02-04 08:11:03 +0700

    comit-14

--- script/script.js
+++ script/script.js
@@ -9,10 +9,27 @@ function layDuLieu() {
     giaTriInput3 == "08112009"
   ) {
     console.log("Dữ liệu từ ô input: " + giaTriInput1);
-    window.location.href =
-      "https://phamquangtruongan.github.io/tailwindcss/";
+    window.location.href = "https://phamquangtruongan.github.io/tailwindcss/";
   } else {
     console.log("Error");
   }
 }
 
+// Optional: Thêm sự kiện để tắt controls khi video đã bắt đầu phát
+// document.querySelector('.rounded-lg').addEventListener('play', function() {
+//   this.controls = false;
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const radioButtons = document.querySelectorAll('input[type="radio"]');
+  const contents = document.querySelectorAll('.content');
+
+  radioButtons.forEach(function(radioButton, index) {
+    radioButton.addEventListener("change", function() {
+      contents.forEach(function(content) {
+        content.classList.remove("active");
+      });
+      contents[index].classList.add("active");
+    });
+  });
+});
